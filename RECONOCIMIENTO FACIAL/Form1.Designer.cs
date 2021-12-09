471623ab8ac49e061e7c355e7e5dc41e08c85a33
Steven Mena <Prodigymax@outlook.es>
2021-12-09 13:04:50 -0400

    Trabajo completo

---
+++ RECONOCIMIENTO FACIAL/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace RECONOCIMIENTO_FACIAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.camara = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Ayuda = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnconsulta = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(443, 272);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(62, 303);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 107);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Camara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 303);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Captura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(512, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(490, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // camara
+            // 
+            this.camara.Location = new System.Drawing.Point(471, 25);
+            this.camara.Name = "camara";
+            this.camara.Size = new System.Drawing.Size(144, 44);
+            this.camara.TabIndex = 6;
+            this.camara.Text = "Abrir camara";
+            this.camara.UseVisualStyleBackColor = true;
+            this.camara.Click += new System.EventHandler(this.camara_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(471, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 44);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Guardar imagen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(471, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 44);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Detectar cara";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(198, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = ".";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Ayuda
+            // 
+            this.Ayuda.Location = new System.Drawing.Point(471, 261);
+            this.Ayuda.Name = "Ayuda";
+            this.Ayuda.Size = new System.Drawing.Size(75, 23);
+            this.Ayuda.TabIndex = 10;
+            this.Ayuda.Text = "Ayuda";
+            this.Ayuda.UseVisualStyleBackColor = true;
+            this.Ayuda.Click += new System.EventHandler(this.Ayuda_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(198, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = ".";
+            // 
+            // btnconsulta
+            // 
+            this.btnconsulta.Location = new System.Drawing.Point(62, 416);
+            this.btnconsulta.Name = "btnconsulta";
+            this.btnconsulta.Size = new System.Drawing.Size(130, 36);
+            this.btnconsulta.TabIndex = 12;
+            this.btnconsulta.Text = "Consulta";
+            this.btnconsulta.UseVisualStyleBackColor = true;
+            this.btnconsulta.Click += new System.EventHandler(this.btnconsulta_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(198, 365);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = ".";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(198, 389);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 24);
+            this.label7.TabIndex = 14;
+            this.label7.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 505);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnconsulta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Ayuda);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.camara);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Reconocimiento facial";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button camara;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Ayuda;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnconsulta;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ RECONOCIMIENTO FACIAL/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FaceRecognition;
+
+namespace RECONOCIMIENTO_FACIAL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FaceRec facerec = new FaceRec();
+
+        private void camara_Click(object sender, EventArgs e)
+        {
+            facerec.openCamera(pictureBox1, pictureBox2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            facerec.Save_IMAGE(textBox1.Text);
+            MessageBox.Show("Se guardo con exito");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            facerec.isTrained = true;  
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            facerec.getPersonName(label4);
+
+        }
+        private void get_person_info()
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { Name = "Steven", ID = 1, Correo = "steven@yes.com", telefono = "8091234569" });
+            people.Add(new Person() { Name = "Gerson", ID = 2, Correo = "gilson@yes.com", telefono = "849122569" });
+            people.Add(new Person() { Name = "Erick", ID = 3, Correo = "erick@yes.com", telefono = "8491534569" });
+            people.Add(new Person() { Name = "Huascar", ID = 4, Correo = "huascar@yes.com", telefono = "8491224669" });
+            people.Add(new Person() { Name = "omar", ID = 5, Correo = "omar@yes.com", telefono = "829123693" });
+
+
+            var filterPerson = people.Where(p => p.Name ==label4.Text);
+            foreach (  var person in filterPerson)
+            {
+                label5.Text = person.ID.ToString();
+                label6.Text = person.Correo.ToString();
+                label7.Text = person.telefono.ToString();
+            }
+            
+        }
+
+        private void Ayuda_Click(object sender, EventArgs e)
+        {
+            string message = "Para usar el programa:1.Camara ,este boton es para abrir la camara,2.Guardar imagen,este boton se usa para guardar al usuario,3.Detectar cara, se utiliza para detectar la cara seleccionada, 4.Consulta, se utiliza para ver los datos de la persona detectada"; 
+            string title = "ayuda";
+            MessageBox.Show(message, title);
+        }
+
+
+        private void btnconsulta_Click(object sender, EventArgs e)
+        {
+            get_person_info();
+
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Correo { get; set; }
+        public string telefono { get; set; }
+
+
+
+
+    }
+}
+ 
